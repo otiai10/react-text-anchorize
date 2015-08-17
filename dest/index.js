f4aa1b2557cbfff502eaab000673137c98d43a42
otiai10 <otiai10@gmail.com>
2015-08-17 21:25:23 +0900

    Fix

--- dest/index.js
+++ dest/index.js
@@ -1,48 +1,61 @@
 var AnchorizableText = React.createClass({displayName: "AnchorizableText",
-    // render it first
-    render: function () {
-        return React.createElement('div', {
-            ref: 'ATSelf'
-        }, this.props.text);
-    },
-    // anchorize it after mount
-    componentDidMount: function() {
-        this.anchorize();
-    },
-    // anchorize it after update
-    componentDidUpdate: function() {
-        this.anchorize();
-    },
-    // anchorize execution
-    anchorize: function() {
-        var myself = React.findDOMNode(this.refs.ATSelf);
-        var value = this.props.text;
-        var self = this;
-        this.props.ExprWrappers.map(function(ew) {
-           value = self.exprAndWrap(value, ew);
-        });
-        myself.innerHTML = value;
-    },
-    // expr and wrap
-    exprAndWrap: function(value, ew /* interface ExprWrapper */) {
-        if (typeof ew.expr != 'function' || typeof ew.wrap != 'function') return value;
-        (ew.expr().exec(value) || []).map(function(found) {
-            value = value.split(found).join(ew.wrap(found));
-        });
-        return value;
-    },
-    getDefaultProps: function() {
-        var sampleExprWrapper = {
-            expr: function () /* RegExp */ {
-                return /おっぱい/gi;
-            },
-            wrap: function (value) /* string */ {
-                return '<span style="background-color:yellow">' + value + '</span>';
+    getInitialState: function() {
+        this.props.rules = this.props.rules || [];
+        var replacers = this.expandByRules([this.props.text]);
+        var contents = [];
+        replacers.forEach(function(replacer, i) {
+            if (typeof replacer === 'string')
+              return contents.push(React.createElement("span", null, replacer));
+            if (typeof replacer.replace === 'function') {
+              setTimeout(function() {
+                replacer.replace.bind(this)(i, replacer.value);
+              }.bind(this));
             }
-        };
-        return {
-            ExprWrappers: [sampleExprWrapper]
-        };
+            var _c = replacer.wrap.bind(this)(replacer.value);
+            return contents.push(React.createElement("span", null, _c));
+        }.bind(this));
+        return {contents: contents};
+    },
+    render: function() {
+        return React.createElement("span", null, this.state.contents);
+    },
+    expandByRules: function(tokens) {
+        if (!this.props.rules) return tokens;
+        this.props.rules.forEach(function(rule, i) {
+            tokens = this.expandByRule(rule, tokens);
+        }.bind(this));
+        return tokens;
+    },
+    expandByRule: function(rule, tokens) /* tokens */ {
+      return (function(tokens) {
+        var expr = new RegExp(rule.match);
+        var res = [];
+        tokens.forEach(function(token) {
+          if (!token.split) return res.push(token); // this is already replacer.
+          token.split(expr).forEach(function(e) {
+            if (e.length === 0) return;
+            if (e.match(expr)) {
+              var r = new AnchorizableText.Replacer(e);
+              if (typeof rule.wrap === 'function') r.wrap = rule.wrap;
+              if (typeof rule.replace === 'function') r.replace = rule.replace;
+              return res.push(r);
+            }
+            return res.push(e);
+          });
+        });
+        return res;
+      })(tokens);
+    },
+    replaceContentsOf: function(index, content) {
+        this.state.contents[index] = content;
+        this.setState({contents: this.state.contents});
     }
 });
 
+AnchorizableText.Replacer = function(substr) {
+  this.value = substr;
+  // default wrap
+  this.wrap = function(sub) {
+    return React.createElement("a", {href: sub, target: "_blank"}, sub);
+  };
+};
